afc4339349ab057b7d8e3e77360ec55badf4ccc1
haynal <haynal@haynal-PC>
2018-01-19 10:11:20 -0500

    Done background

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,20 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.mygdx.game.Background;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+
+    public static void main(String[] arg) {
+
+        // place the new image with an atlas into a folder called blocks
+        TexturePacker.process("Background", "blocks", "blocks");
+
+        // used to start the desktop version of the game
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 800;
+        config.height = 600;
+        new LwjglApplication(new Background(), config);
 	}
 }
